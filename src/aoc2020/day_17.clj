965bbe1494e46f0940c377d65023c602efee2e48
John Flinchbaugh <john@hjsoft.com>
2020-12-18 15:14:36 -0500

    day 17 part 2

--- src/aoc2020/day_17.clj
+++ src/aoc2020/day_17.clj
@@ -16,26 +16,47 @@
 (defn parse [input]
   (->> input str/trim str/split-lines (map seq)))
 
-(defn to-cube [lsts]
+(defn to-cube [ndims lsts]
   (->> (for [y (range (count lsts))
              x (range (count (first lsts)))]
-         (when (= \# (nth (nth lsts y) x)) [x y 0]))
+         (when (= \# (nth (nth lsts y) x))
+           (vec (concat [x y] (repeat (- ndims 2) 0)))))
        (filter identity)
        set))
 
-(defn surrounding-positions [[ox oy oz]]
-  (remove #{[ox oy oz]}
-          (for [x (range (dec ox) (+ 2 ox))
-                y (range (dec oy) (+ 2 oy))
-                z (range (dec oz) (+ 2 oz))]
-            [x y z])))
+(defn surrounding-positions [pos]
+  (let [ndims (count pos)
+        [ox oy oz] pos]
+    (remove #{pos}
+      (for [x (range (dec ox) (+ 2 ox))
+            y (range (dec oy) (+ 2 oy))
+            z (range (dec oz) (+ 2 oz))]
+        [x y z]))))
+
+(defn surrounding-positions-4 [pos]
+  (let [ndims (count pos)
+        [ox oy oz ow] pos]
+    (remove #{pos}
+      (for [x (range (dec ox) (+ 2 ox))
+            y (range (dec oy) (+ 2 oy))
+            z (range (dec oz) (+ 2 oz))
+            w (range (dec ow) (+ 2 ow))]
+        [x y z w]))))
 
 (defn count-surrounding [cube pos]
-  (let [surroundings (filter identity (map cube (surrounding-positions pos)))]
+  (let [ndims (count pos)
+        sur-pos (case ndims
+                  3 surrounding-positions
+                  4 surrounding-positions-4)
+        surroundings (filter identity (map cube (sur-pos pos)))]
     (count surroundings)))
 
 (defn candidate-positions [cube]
-  (->> cube (map surrounding-positions) (reduce concat cube) set))
+  (let [ndims (-> cube first count)
+        sur-pos (case ndims
+                  3 surrounding-positions
+                  4 surrounding-positions-4)]
+    (->> cube (map sur-pos) (reduce concat cube) set)))
 
 (defn next-val [cube pos]
   (let [cnt (count-surrounding cube pos)
@@ -54,11 +75,17 @@
     set))
 
 (defn part-1 []
-  (->> input parse to-cube (iterate next-cube) (take (inc 6)) last count))
+  (->> input parse (to-cube 3) (iterate next-cube) (take (inc 6)) last count))
+
+(defn part-2 []
+  (->> input parse (to-cube 4) (iterate next-cube) (take (inc 6)) last count))
 
 (comment
 
   (part-1)
 ;; => 380
+
+  (part-2)
+;; => 2332
 
   )
